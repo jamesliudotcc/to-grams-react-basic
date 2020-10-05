7f22d372f7044d1f00b0f8516f1e73c347699a11
James Liu <james@pop-os.localdomain>
2020-10-04 19:03:13 -0700

    calculation works

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fromJS, Map } from "immutable";
+import "./App.css";
+
+const GRAMS_IN_OZ = 29.5735;
+
+const UNITS = fromJS({
+  cup: { name: "Cup", inOz: 8 },
+  fl_oz: {
+    name: "Ounce (Fluid)",
+    inOz: 1,
+    volume: true,
+  },
+  tbsp: {
+    name: "Tablespoon",
+    inOz: 0.5,
+    volume: true,
+  },
+  tsp: {
+    name: "Teaspoon",
+    inOz: 1 / 6,
+    volume: true,
+  },
+});
+
+const INGREDIENTS = fromJS({
+  water: { name: "Water", density: 1 },
+  oil: { name: "Oil", density: 0.917 },
+  butter: { name: "Butter", density: 0.911 },
+  flour: { name: "Flour", density: 0.531 },
+  sugar: { name: "Sugar", density: 0.845 },
+  brown_sugar: { name: "Brown Sugar", density: 0.93 },
+  bPowder: { name: "Baking Powder", density: 0.972 },
+  bSoda: { name: "Baking Soda", density: 0.974 },
+  cchips: { name: "Chocolate Chips", density: 0.634 },
+  yeast: { name: "Yeast", density: 0.658 },
+});
 
 function App() {
+  const [unit, setUnit] = useState("cup");
+  const [ingredient, setIngredient] = useState("water");
+  const [toConvert, setToConvert] = useState("0");
+
+  const handleUnitChange = (e: any) => {
+    setUnit(e.target.value);
+  };
+
+  const handleIngredientChange = (e: any) => {
+    setIngredient(e.target.value);
+  };
+
+  const handleToConvertChange = (e: any) => {
+    setToConvert(e.target.value);
+  };
+
+  const inGrams = (unit: string, ingredient: string, toConvert: string) =>
+    String(
+      Math.round(
+        Number(toConvert) *
+          GRAMS_IN_OZ *
+          UNITS.getIn([unit, "inOz"]) *
+          INGREDIENTS.getIn([ingredient, "density"])
+      ) || ""
+    );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={toConvert}
+        onChange={handleToConvertChange}
+        type="number"
+      ></input>
+      <select value={unit} onChange={handleUnitChange}>
+        {UNITS.map((v: any, k: any) => (
+          <option value={k}>{v.get("name")}</option>
+        ))}
+      </select>
+      <select value={ingredient} onChange={handleIngredientChange}>
+        {INGREDIENTS.map((v: any, k: any) => (
+          <option value={k}>{v.get("name")}</option>
+        ))}
+      </select>
+      {inGrams(unit, ingredient, toConvert)}
     </div>
   );
 }
