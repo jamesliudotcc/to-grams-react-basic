003edc89b01cb006ead48aea592e88fc7b605a7b
James Liu <james@jamesliu.cc>
2021-01-25 19:35:12 -0800

    Updates

---
+++ precache-manifest.de5a722ea2ac62dd7219fd3ad8a1a66b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bacb5136ce8f120021605478ba253f1",
+    "url": "/to-grams-react-basic/index.html"
+  },
+  {
+    "revision": "4489376f90f9b85ddc4d",
+    "url": "/to-grams-react-basic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "21dcc5975e3c4361a7aa",
+    "url": "/to-grams-react-basic/static/js/2.64b6cbfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-grams-react-basic/static/js/2.64b6cbfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4489376f90f9b85ddc4d",
+    "url": "/to-grams-react-basic/static/js/main.bdb58456.chunk.js"
+  },
+  {
+    "revision": "6f0619a68a1b722165f0",
+    "url": "/to-grams-react-basic/static/js/runtime-main.800bf666.js"
+  }
+]);
